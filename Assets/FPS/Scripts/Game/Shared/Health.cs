6de2f396b30ece25a9f0412baae0e1363449cba9
syenbu <118796496+syenbu@users.noreply.github.com>
2026-05-23 18:17:47 -0700

    linked eye to health (sanity)

--- Assets/FPS/Scripts/Game/Shared/Health.cs
+++ Assets/FPS/Scripts/Game/Shared/Health.cs
@@ -83,5 +83,7 @@ namespace Unity.FPS.Game
                 OnDie?.Invoke();
             }
         }
+
+        
     }
 }
---
+++ Assets/_DarkMoon/PawwStuff/Scripts/eyeIndicatorAnimation.cs
@@ -0,0 +1,115 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.FPS.Game;
+
+using UnityEngine.AI;
+using UnityEngine.Events;
+
+
+public class eyeIndicatorAnimation : MonoBehaviour
+{
+
+
+    public float CurrentHealth { get; set; }
+    private Animator animator;
+    public UnityAction onIsanityDamaged;
+
+    public Health m_Health;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+        
+        DebugUtility.HandleErrorIfNullGetComponent<Health, eyeIndicatorAnimation>(m_Health, this, gameObject);
+        animator = GetComponent<Animator>();
+        m_Health.OnDamaged += OnDamaged;
+
+
+
+        //CurrentHealth = MaxHealth;
+        animator.SetBool("Insane", false);
+        animator.SetBool("VeryInsane", false);
+        animator.SetBool("Wary", false);
+
+        
+
+    }
+
+
+
+    void OnDamaged(float damage, GameObject damageSource)
+    {
+        // test if the damage source is the player
+
+        checkInsanity(m_Health.CurrentHealth);
+        Debug.Log(m_Health.CurrentHealth);  
+
+        onIsanityDamaged?.Invoke();
+        
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Start the coroutine
+        //StartCoroutine(WaitAndExecute());
+        
+        //checkInsanity(CurrentHealth);
+    }
+
+    // Must return IEnumerator
+    IEnumerator WaitAndExecute()
+    {
+        Debug.Log("Waiting started...");
+
+        // Pause execution for 5 seconds
+        yield return new WaitForSeconds(2f);
+
+        Debug.Log("5 seconds have passed!");
+
+        // Put your post-delay code here (e.g., turn off an animation parameter)
+    }
+
+
+    void checkInsanity(float currentHealth)
+    {
+        if (currentHealth < 30)
+        {
+            Debug.Log("you are low sanity!!!" + currentHealth);
+            // very low sanity
+            animator.SetBool("Insane", true);
+            animator.SetBool("VeryInsane", true);
+            animator.SetBool("Wary", true);
+
+        }
+        else if (currentHealth < 60)
+        {
+            // low sanity
+            animator.SetBool("Insane", true);
+            animator.SetBool("VeryInsane", false);
+            animator.SetBool("Wary", true);
+        }
+
+        else if (CurrentHealth < 80)
+        {
+            // wary
+            animator.SetBool("Insane", false);
+            animator.SetBool("VeryInsane", false);
+            animator.SetBool("Wary", true);
+        }
+        else
+        {
+            // sane
+            animator.SetBool("Insane", false);
+            animator.SetBool("VeryInsane", false);
+            animator.SetBool("Wary", false);
+        }
+    }
+}
